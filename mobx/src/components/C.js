e8a8af3b1689a29beb4cba9771010b8d8827c5a7
yehj <hjye2@iflytek.com>
2021-05-07 18:06:37 +0800

    监听数据变化

--- mobx/src/components/C.js
+++ mobx/src/components/C.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { observer, inject } from 'mobx-react';
+import { autorun, reaction, when } from 'mobx';
 function C({ count }) {
   console.log('C');
+  useEffect(() => {
+    autorun((reaction) => {
+      console.log(count.count);
+      count.count > 1 && reaction.dispose(); // 终止监听
+    });
+    reaction(
+      () => {
+        return count.count;
+      },
+      (count, old, reaction) => {
+        console.log(count, old);
+        count > 3 && reaction.dispose(); // 终止监听
+      }
+    );
+    when(
+      () => {
+        return count.count > 2;
+      },
+      () => {
+        console.log(count.count);
+      }
+    );
+  }, []);
   return (
     <div>
       <div>count: {count.count}</div>
